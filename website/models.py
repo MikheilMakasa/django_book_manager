063713fac30b8a0cdca46cece06a3cda4ecfc9a0
Mikheil Makasarashvili <makasarashvilimikheil@gmail.com>
2024-06-21 14:31:11 +0400

    added table

--- website/models.py
+++ website/models.py
@@ -1,3 +1,17 @@
+
+
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	book_name = models.CharField(max_length=50)
+	book_author =  models.CharField(max_length=50)
+	year = models.CharField(max_length=15)
+	description =  models.CharField(max_length=500)
+	score =  models.CharField(max_length=15)
+	publisher =  models.CharField(max_length=100)
+	
+
+	def __str__(self):
+		return(f"{self.book_name}, {self.book_author}, {self.year}")
